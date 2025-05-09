8f8e6d6ca3f19a535bc8cded89d05ec95cdb9f79
tanna-1 <tan.nayir@student.ucll.be>
2025-05-09 15:01:46 +0200

    Implemented proper value scaling for hypernet

--- advanced_ai_project/hypernet/utils.py
+++ advanced_ai_project/hypernet/utils.py
@@ -4,6 +4,32 @@ import torch.nn as nn
 
 from ..model import OUT_DIM, MLPModel
 
+"""
+If the output of the hypernet is too far from the distribution of initialized CNN weights
+the model is unable to converge, as it leads to exploding gradients within the CNN.
+
+The problem likely could've been solved by implementing the initialization method in the paper
+"Chang, O., Flokas, L., & Lipson, H. (2023). Principled weight initialization for hypernetworks. arXiv. https://doi.org/10.48550/arXiv.2312.08399".
+
+However I opted for a simpler solution where the hypernet output is scaled
+by a constant value to match the distribution of the CNN weights.
+
+Measured distribution of the hypernet output:
+    - Mean: 0.005435472819954157
+    - Std: 0.6838817596435547
+    - Max: 3.3123366832733154
+    - Min: -3.272418975830078
+
+Measured distribution of the CNN weights:
+    - Mean: -0.0027166458312422037
+    - Std: 0.03464247286319733
+    - Max: 0.28350767493247986
+    - Min: -0.29618480801582336
+
+The value is the ratio of the standard deviations of the two distributions.
+"""
+HYPERNET_OUTPUT_SCALE = 0.034 / 0.68
+
 
 def generate_weight_info(modules_dict: dict[str, nn.Module]):
     """
@@ -37,11 +63,11 @@ def generate_weight_info(modules_dict: dict[str, nn.Module]):
 def compute_weight_tensor(model: MLPModel, param_count: int) -> torch.Tensor:
     """
     Compute a weight tensor from the model output.
-    
+
     Args:
         model (MLPModel): The MLP model to generate weights from.
         param_count (int): The total number of parameters needed.
-        
+
     Returns:
         torch.Tensor: A flattened tensor of normalized weights.
     """
@@ -51,10 +77,9 @@ def compute_weight_tensor(model: MLPModel, param_count: int) -> torch.Tensor:
         dtype=torch.int64,
         device=model.device,
     )
-    weight_vectors = model(index_tensor)
+    weight_tensor = model(index_tensor)
 
-    # Normalize the weight vectors
-    weight_vectors = torch.tanh(weight_vectors)
+    # Scale the weights so that they have the correct variance
+    weight_tensor = weight_tensor * HYPERNET_OUTPUT_SCALE
 
-    # Flatten the weight vectors
-    return weight_vectors.flatten()
+    return weight_tensor.flatten()
